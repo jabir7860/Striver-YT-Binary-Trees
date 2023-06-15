e5a0d50cff5e32d9a72225481a7e102e6816abbe
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-06-15 22:40:06 +0530

    Update 24-Flatten a BT to LL.cpp

--- 24-Flatten a BT to LL.cpp
+++ 24-Flatten a BT to LL.cpp
@@ -1,1 +1,52 @@
+// STRIVER
+TreeNode<int> *prev=NULL; 
+void fun(TreeNode<int> *root) 
+{
+    if(root==NULL) return ; 
+    fun(root->right); 
+    fun(root->left); 
+    root->right=prev; 
+    root->left=NULL; 
+    prev=node; 
+}
+TreeNode<int> *flattenBinaryTree(TreeNode<int> *root) {
+    if(root==NULL) return NULL; 
+    fun(root); 
+    return root; 
+}
+// MORRIS
+TreeNode<int> *flattenBinaryTree(TreeNode<int> *root)
 
+{
+
+    TreeNode<int>* curr = root;
+
+    while(curr!=NULL){
+
+        if(curr->left){
+
+        TreeNode<int>* pred = curr->left;
+
+            while(pred->right){
+
+                pred = pred->right;
+
+            }
+
+            pred->right=curr->right;
+
+            curr->right=curr->left;
+
+            curr->left=NULL;
+
+    }
+
+    curr=curr->right;
+
+    }
+
+    return root;
+
+} 
+
+// 
