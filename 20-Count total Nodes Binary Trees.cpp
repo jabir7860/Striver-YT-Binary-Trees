fa738934c389eaa286984dcaa35f0a7a83fdf1a5
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-06-16 22:15:42 +0530

    Update 20-Count total Nodes Binary Trees.cpp

--- 20-Count total Nodes Binary Trees.cpp
+++ 20-Count total Nodes Binary Trees.cpp
@@ -1,1 +1,8 @@
 https://leetcode.com/problems/count-complete-tree-nodes/
+
+ int countNodes(TreeNode* root) 
+    {
+        if(root==NULL) return 0; 
+        
+        return 1+countNodes(root->left)+countNodes(root->right); 
+    }
