076a7ba93229b29de6dfb1fda3222429ff19157b
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-06-17 10:37:03 +0530

    Update 17-Child SUm Proerty.cpp

--- 17-Child SUm Proerty.cpp
+++ 17-Child SUm Proerty.cpp
@@ -1,6 +1,13 @@
-stiver point of view is different 
-see video 
+// Check Whether Given Binary Tree Follows Child Sum Property or Not.
 
-
-https://practice.geeksforgeeks.org/problems/children-sum-parent/1 
-// another one 
+ int isSumProperty(Node *root)
+    {
+        if(root==NULL || (root->left==NULL && root->right==NULL)) return 1;
+        int sum; 
+        if(root->left==NULL) sum=root->right->data; 
+        if(root->right==NULL) sum=root->left->data;  
+        if(root->left!=NULL && root->right!=NULL) sum=root->left->data+root->right->data; 
+        if(root->data!=sum) return 0; 
+        if(isSumProperty(root->left)==1 && isSumProperty(root->right)==1) return 1; 
+        else return 0; 
+    }
