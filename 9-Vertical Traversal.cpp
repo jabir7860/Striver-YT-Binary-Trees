fc8afe5fc64c6d3e216fc6df149abac48815e74b
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-06-17 15:15:07 +0530

    Update 9-Vertical Traversal.cpp

--- 9-Vertical Traversal.cpp
+++ 9-Vertical Traversal.cpp
@@ -1,3 +1,38 @@
 https://practice.geeksforgeeks.org/problems/print-a-binary-tree-in-vertical-order/1 
 
 https://leetcode.com/problems/vertical-order-traversal-of-a-binary-tree/
+
+
+vector<vector<int>> verticalTraversal(TreeNode* root) {
+      queue<pair<TreeNode*,pair<int,int>>>q;
+map<int,map<int,multiset<int>>>mp;
+q.push({root,{0,0}});
+while(!q.empty())
+{
+auto it=q.front();
+q.pop();
+TreeNode*node=it.first;
+int x=it.second.first;
+int y=it.second.second;
+mp[x][y].insert(node->val);
+if(node->left)
+{
+q.push({node->left,{x-1,y+1}});
+}
+if(node->right)
+{
+q.push({node->right,{x+1,y+1}});
+}
+}
+vector<vector<int>>ans;
+for(auto it:mp)
+{
+vector<int>v;
+for(auto ik:it.second)
+{
+v.insert(v.end(),ik.second.begin(),ik.second.end());
+}
+ans.push_back(v);
+}
+return ans;
+    }
