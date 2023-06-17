dc3de1dbef7e7d529cd050a6e12d1ea9f0b188ef
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-06-17 15:07:59 +0530

    Update 3-Daimeter of BT.cpp

--- 3-Daimeter of BT.cpp
+++ 3-Daimeter of BT.cpp
@@ -1,1 +1,29 @@
 https://leetcode.com/problems/diameter-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int fun(TreeNode* root,int &ans) 
+    {
+        if(root==NULL) return 0; 
+        int left=fun(root->left,ans); 
+        int right=fun(root->right,ans); 
+        ans=max(ans,left+right); 
+        return 1+max(left,right); 
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int ans=INT_MIN; 
+        fun(root,ans); 
+        return ans; 
+    }
+}; 
