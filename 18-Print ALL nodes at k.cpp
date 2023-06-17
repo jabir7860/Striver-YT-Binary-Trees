dcf3c8cd00e9729a1e95c83314f1498506208868
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-06-17 10:17:19 +0530

    Update 18-Print ALL nodes at k.cpp

--- 18-Print ALL nodes at k.cpp
+++ 18-Print ALL nodes at k.cpp
@@ -1,4 +1,64 @@
-https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/ 
-Solve this one also. 
-// Similar Problem 
-https://practice.geeksforgeeks.org/problems/9b61b8efbb030aed799055f776629dbf1287fbae/1 
+// 
+    void fun(TreeNode* root,unordered_map<TreeNode*,TreeNode*>&ParentNodes)
+    {
+        queue<TreeNode*>q; 
+        q.push(root); 
+        while(!q.empty())
+        {
+            TreeNode* k=q.front(); 
+            q.pop(); 
+            if(k->left) 
+            {
+                ParentNodes[k->left]=k; 
+                q.push(k->left); 
+            }
+            if(k->right)
+            {
+                ParentNodes[k->right]=k; 
+                q.push(k->right); 
+            }
+        }        
+    }
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*,TreeNode*>ParentNodes; 
+        fun(root,ParentNodes);  
+        unordered_map<TreeNode*,bool>vis; 
+        queue<TreeNode*>q; 
+        q.push(target); 
+        vis[target]=1; 
+        int curr_size=0; 
+        while(!q.empty())
+        {
+            int size=q.size(); 
+            if(curr_size++ == k) break; 
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* temp=q.front(); 
+                q.pop(); 
+                if(temp->left && !vis[temp->left])
+                {
+                    vis[temp->left]=1; 
+                    q.push(temp->left); 
+                }
+                if(temp->right && !vis[temp->right])
+                {
+                    vis[temp->right]=1; 
+                    q.push(temp->right); 
+                }
+                if(ParentNodes[temp] && !vis[ParentNodes[temp]])
+                {
+                    vis[ParentNodes[temp]]=1; 
+                    q.push(ParentNodes[temp]); 
+                }
+            }
+        } 
+        vector<int>ans; 
+        while(!q.empty())
+        {
+            TreeNode* x=q.front(); 
+            q.pop(); 
+            ans.push_back(x->val); 
+        }
+        return ans; 
+    }
+};
