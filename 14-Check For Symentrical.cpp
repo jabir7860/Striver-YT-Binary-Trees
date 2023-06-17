75d725d2503d209ed281873fbdb733403e27a0c7
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-06-17 10:51:23 +0530

    Update 14-Check For Symentrical.cpp

--- 14-Check For Symentrical.cpp
+++ 14-Check For Symentrical.cpp
@@ -1,1 +1,13 @@
-https://leetcode.com/problems/symmetric-tree/submissions/
+bool fun(TreeNode* l,TreeNode* r)
+    {
+        if(l==NULL && r==NULL) return 1; 
+        if(l==NULL || r==NULL) return 0; 
+        if(l->val!=r->val) return 0; 
+
+        return fun(l->left,r->right) && fun(l->right,r->left); 
+    }
+    bool isSymmetric(TreeNode* root)
+    {
+            if(root==NULL) return 0; 
+            return fun(root->left,root->right);     
+    }
