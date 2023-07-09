4cb344aac5cc74bc8daaee9b91ff85a80f48b376
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-07-09 19:53:50 +0530

    Update 10-Top View of Binary Tree.cpp

--- 10-Top View of Binary Tree.cpp
+++ 10-Top View of Binary Tree.cpp
@@ -1,1 +1,26 @@
 https://practice.geeksforgeeks.org/problems/top-view-of-binary-tree/1
+
+
+ vector<int> topView(Node *root)
+    {
+         vector<int>ans; 
+        if(root==NULL) return ans; 
+        map<int,int>mpp; 
+        queue<pair<Node*,int>>q; 
+        q.push({root,0}); 
+        while(!q.empty())
+        {
+            auto p=q.front(); 
+            q.pop(); 
+            Node* node=p.first; 
+            int line=p.second; 
+            if(mpp.find(line)==mpp.end())
+            {
+                mpp[line]=node->data; 
+            }
+            if(node->left) q.push({node->left,line-1}); 
+            if(node->right) q.push({node->right,line+1}); 
+        }
+        for(auto i:mpp) ans.push_back(i.second); 
+        return ans; 
+    }
