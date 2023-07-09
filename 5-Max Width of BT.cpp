e0fd2793ad5ff5f312470fcfdf887b07461b5987
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-07-09 16:27:24 +0530

    Update 5-Max Width of BT.cpp

--- 5-Max Width of BT.cpp
+++ 5-Max Width of BT.cpp
@@ -1,21 +1,23 @@
 https://leetcode.com/problems/maximum-width-of-binary-tree/ 
+
+
  int widthOfBinaryTree(TreeNode* root) {
-         if(!root)return 0;
+          if(!root)return 0;
         int ans=0;
         queue<pair<TreeNode*,int>> q;
 
         q.push({root,0});
-        while(!q.empty()){
+        while(!q.empty())
+        {
             int size=q.size();
-            auto top=q.front();
-            int min=top.second;
             int first,last;
-            for(int i=0;i<size;i++){
-                int cur_id=q.front().second-min;
+            for(int i=0;i<size;i++)
+            {
                 TreeNode* node=q.front().first;
-                q.pop();
-                if(i==0)first=cur_id;
-                if(i==size-1)last=cur_id;
+                int cur_id=q.front().second; 
+                if(i==0)first=q.front().second; 
+                if(i==size-1)last=q.front().second; 
+                q.pop(); 
 
                 if(node->left){
                     q.push({node->left,(long long)2*cur_id+1});
@@ -27,4 +29,4 @@ https://leetcode.com/problems/maximum-width-of-binary-tree/
             ans=max(ans,last-first+1);
         }
         return ans;
-    }
+    } 
