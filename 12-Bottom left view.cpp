ced56b5c31f9e85a74450489b4b25a2b4e1990c8
MOHAMMAD JABIR <79660697+jabir7860@users.noreply.github.com>
2023-07-09 21:02:19 +0530

    Update 12-Bottom left view.cpp

--- 12-Bottom left view.cpp
+++ 12-Bottom left view.cpp
@@ -1,1 +1,20 @@
 https://leetcode.com/problems/find-bottom-left-tree-value/
+
+int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*>q; 
+        q.push(root); 
+        int ans=-1; 
+        while(!q.empty()) 
+        {
+            int n=q.size(); 
+            for(int i=0;i<n;i++) 
+            {
+                auto it=q.front(); 
+                q.pop(); 
+                if(i==0) ans=it->val; 
+                if(it->left) q.push(it->left); 
+                if(it->right) q.push(it->right); 
+            }
+        }
+        return ans; 
+    }
